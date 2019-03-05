c0f9370f2c2f6ffb8015fd7ebb125398503b96bf
Fedecardozo <47437164+Fedecardozo@users.noreply.github.com>
2019-03-04 21:27:09 -0300

    Practica en casa examen

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -17,55 +17,85 @@ function mostrar()
 	var contadorPaisesImpar;
 	var contadorPaisesMenor;
 	var contadorPaisesIgual;
+	var contadorPaisesMayor;
 	var resto;
 	var contador;
-	var primerPais;
+	var primerPaisMayor;
+	var paisMenor;
+	var sumaKilometros;
+	var promedioSuperficie;
+	var territorioMinimo;
 
 	respuesta="si";
 	territorio=0;
 	contadorPaisesImpar=0;
 	contadorPaisesMenor=0;
 	contadorPaisesIgual=0;
+	contadorPaisesMayor=0;
 	contador=0;
+	sumaKilometros=0;
 
 	while(respuesta!="no")
 	{
 		pais=prompt("Ingrese pais ");
-		territorio=prompt("Ingrese territorio");
+		territorio=prompt("Ingrese superficie");
 		territorio=parseInt(territorio);
 		respuesta=prompt("Quiere ingresar mas paises? ");
 
-		resto= territorio%2;
+		while(territorio<0 || territorio>1000)
+		{
+			territorio=prompt("Error, Ingrese superficie valida entre 0 y 1000");
+			territorio=parseInt(territorio);
+		}
 
-		if(resto==1)
+		sumaKilometros=sumaKilometros + territorio;
+
+		resto= territorio%2;
+	
+
+		if(resto!=0)
 		{
 			contadorPaisesImpar=contadorPaisesImpar+1;
 		}
+		if(territorio<100)
+		{
+			contadorPaisesMenor=contadorPaisesMenor+1;
+		}
 		else
 		{
-			if(territorio<100)
-			{
-				contadorPaisesMenor=contadorPaisesMenor+1;
-			}
 			if(territorio==100)
 			{
 				contadorPaisesIgual=contadorPaisesIgual+1;
 			}
+			if(territorio>100 && contadorPaisesMayor==0)
+			{
+				contadorPaisesMayor=contadorPaisesMayor+1;
+				primerPaisMayor=pais;
+			}
 		}
-
-		contador=contador+1
-
-		if(contador==1)
+		if(contador==0)
 		{
-			pais=primerPais;
+			paisMenor=pais;
+			territorioMinimo=territorio;
 		}
 		else
 		{
-			if(primerPais && territorio>100)
-		}		
+			if(territorio<territorioMinimo)
+			{
+				territorioMinimo=territorio;
+				paisMenor=pais;
+			}
+		}
+		contador=contador+1;		
 	}
 
+	promedioSuperficie=sumaKilometros/contador;
 
-
+	document.write("1-La cantidad de países con superficie impar.= " + contadorPaisesImpar + "<br>" +
+	"2-La cantidad de países con superficie menor a 100= " + contadorPaisesMenor + "<br>" +
+	"3-La cantidad de países con superficie igual a 100= " + contadorPaisesIgual + "<br>" +
+	"4-El nombre del primer país que superó los 100 de superficie= " + primerPaisMayor + "<br>" +
+	"5-El promedio de kilómetros ingresados.= " + promedioSuperficie + "<br>" +
+	"6-El nombre del que menos territorio tiene= " + paisMenor + " y su territorio es: " + territorioMinimo);
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
@@ -1,4 +1,40 @@
+/*4-Pedir dos numeros y mostrar el el resultado:
+Si son iguales los muestro concatenados. Si el primero es mayor,los resto, de lo contrario los sumo.
+Si la suma es mayor a 10, ademas de mostrar el resultado, muestro el mensaje "la suma es xxx y supero el 10"
+usamos IF*/
+
 function mostrar()
 {
+	var numero;
+	var numeroDos;
+	var resta;
+	var suma;
+	var mensaje;
 
+	numero=prompt("Ingrese numero");
+	numeroDos=prompt("Ingrese segundo numero");
+	numero=parseInt(numero);
+	numeroDos=parseInt(numeroDos);
+
+	if(numero==numeroDos)
+	{
+		mensaje=(" Numero uno" + numero + " numero dos " + numeroDos);
+	}
+	else
+	{
+		if(numero>numeroDos)
+		{
+			resta=numero - numeroDos;
+			mensaje=" La resta es " + resta;
+		}
+		else
+		{
+			suma=numero + numeroDos;
+		}
+	}
+	if(suma>10)
+	{
+		mensaje=" La suma es " + suma + " y supero el 10 ";
+	}
+	alert(mensaje);
 }
